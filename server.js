d84ce0f7727d0951a1b854c15ed7de9344ffbfce
Aristopoulos <aristos.stylianides@cytanet.com.cy>
2026-05-06 15:58:56 +0300

    allagi server

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./db");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("UniBite server is running!");
+  res.send("UniBite server is running!");
+});
+
+// TEST API: παίρνει όλα τα meals
+app.get("/api/meals", (req, res) => {
+  const sql = "SELECT * FROM meals";
+
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Database error" });
+      return;
+    }
+
+    res.json(results);
+  });
 });
 
 app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+  console.log("Server running on http://localhost:3000");
 });
