3883a6115ceeab62739a1c2472196856f41f4c7d
Artemios99 <artemios.kousiappas3@gmail.com>
2026-06-01 20:12:38 +0300

    Teliko

--- server.js
+++ server.js
@@ -594,6 +594,6 @@ app.get("/api/admin/dashboard", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
 });
